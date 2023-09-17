53c4bff6fab016921db1d1411b08ca680102127a
“psubhajit14” <psubhajit14@gmail.com>
2023-09-17 21:07:18 +0530

    server api fixed with language support

--- server/routes/news.js
+++ server/routes/news.js
@@ -4,13 +4,13 @@ const { parse } = require('node-html-parser');
 const puppeteer = require("puppeteer");
 
 const searchApiTopicWise = {
-    "Sports": "https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKSlRpZ0FQAQ?hl=bn&gl=IN&ceid=IN:bn",
-    "India": "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRE55YXpBU0FtSnVLQUFQAQ?hl=bn&gl=IN&ceid=IN%3Abn",
-    "Business": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtSnVHZ0pKVGlnQVAB?hl=bn&gl=IN&ceid=IN%3Abn",
-    "Entertainment": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtSnVHZ0pKVGlnQVAB?hl=bn&gl=IN&ceid=IN%3Abn",
-    "World": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtSnVHZ0pKVGlnQVAB?hl=bn&gl=IN&ceid=IN%3Abn"
+    "Sports": "https://news.google.com/topics/CAAqKggKIiRDQkFTRlFvSUwyMHZNRFp1ZEdvU0JXVnVMVWRDR2dKSlRpZ0FQAQ",
+    "India": "https://news.google.com/topics/CAAqIQgKIhtDQkFTRGdvSUwyMHZNRE55YXpBU0FtSnVLQUFQAQ",
+    "Business": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtSnVHZ0pKVGlnQVAB",
+    "Entertainment": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNREpxYW5RU0FtSnVHZ0pKVGlnQVAB",
+    "World": "https://news.google.com/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx1YlY4U0FtSnVHZ0pKVGlnQVAB"
 }
-
+const queryBuilder = (lang) => `?hl=${lang}&gl=IN&ceid=IN:${lang}`
 const baseUrl = 'https://news.google.com/';
 const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
@@ -34,18 +34,18 @@ class News {
 const getDetails = (element) => {
     const title = element.querySelector('h4').textContent;
     const thumbnail = element.querySelector('figure > img').getAttribute('src');
-    const publisherLogo = element.querySelector('div > img').getAttribute('src');
+    const publisherLogo = element.querySelector('div > img').getAttribute('srcSet');
     const publisher = element.querySelector('div > img').nextSibling.textContent;
     const publishedAt = element.querySelector('time').textContent;
     var url = element.querySelector('a').getAttribute('href');
-    const news = new News(title, publisherLogo, thumbnail, publisher, publishedAt, url);
+    const news = new News(title, thumbnail, publisherLogo, publisher, publishedAt, url);
     if (url)
         return news;
     return new News();
 }
 
-const crawlGoogleLink = async (topic) => {
-    const googleLink = searchApiTopicWise[topic];
+const crawlGoogleLink = async (topic, language) => {
+    const googleLink = searchApiTopicWise[topic] + queryBuilder(language);
     const response = await fetch(googleLink, { headers: headers })
     const body = await response.text();
     const html = parse(body)
@@ -60,10 +60,18 @@ const crawlGoogleLink = async (topic) => {
             console.log("related", relatedArticles.length)
             relatedArticles.forEach((itemNode) => {
                 const title = itemNode.querySelector('h4').textContent;
-                const publisherLogo = itemNode.querySelector('img').getAttribute('src');
-                const publisher = itemNode.querySelector('img').nextSibling.textContent;
+                let publisherLogo = ""
+                let publisher = ""
+                if (itemNode.querySelector('img')) {
+                    publisherLogo = itemNode.querySelector('img').getAttribute('srcSet');
+                    publisher = itemNode.querySelector('img').nextSibling.textContent;
+                }
                 const publishedAt = itemNode.querySelector('time').textContent;
-                relatedNews.push({ title, publisherLogo, publisher, publishedAt })
+                let url = itemNode.querySelectorAll('a')[0].getAttribute('href')
+                if (url.startsWith('./')) {
+                    url = baseUrl + url.split('./')[1]
+                }
+                relatedNews.push({ title, publisherLogo, publisher, publishedAt, url })
             })
         }
         const mainNews = getDetails(mainArticle)
@@ -77,7 +85,8 @@ const crawlGoogleLink = async (topic) => {
 router.get('/feed/:topic', async (req, res) => {
     try {
         const topic = req.params['topic'];
-        const result = await crawlGoogleLink(topic);
+        const language = req.query['lang'];
+        const result = await crawlGoogleLink(topic, language);
         res.status(200).json(result)
     } catch (error) {
         if (error) {
